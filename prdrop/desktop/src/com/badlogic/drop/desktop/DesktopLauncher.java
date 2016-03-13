df9c526016e741079835c8b46d2a0bbe52dafd2d
Skylark <Skylark1934@yandex.ru>
2016-03-13 22:31:58 +0300

    Simple libgdx game

---
+++ prdrop/desktop/src/com/badlogic/drop/desktop/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.badlogic.drop.desktop;
+
+import com.badlogic.drop.Drop;
+import com.badlogic.drop.GameScreen;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Drop(), config);
+		config.title = "Drop";
+		config.width= 800;
+		config.height = 480;
+
+	}
+}
